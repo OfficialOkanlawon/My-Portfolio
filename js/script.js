119a39e1ca62f41b7a1256113d118009d1479f05
OfficialOkanlawon <okanlawonimmanuel@gmail.com>
2022-09-17 09:43:16 +0100

    My personal portfolio

--- js/script.js
+++ js/script.js
@@ -38,3 +38,22 @@ navitems.forEach(item => {
     })
 
 })
+
+window.addEventListener('scroll', show);
+
+function show(){
+    let shows = document.querySelectorAll('.showw');
+
+    for(let i = 0; i < shows.length; i++){
+
+        let windowheight = window.innerHeight;
+        let showtop = shows[i].getBoundingClientRect().top;
+        let showpoint = 150;
+
+        if(showtop < windowheight - showpoint){
+            shows[i].classList.add('active');
+        }else{
+            shows[i].classList.remove('active');
+        }
+    }
+}
